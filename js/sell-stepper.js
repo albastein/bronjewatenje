2bcc128e7a5574841a31aa5df4ea8e7a325d674c
drakeocholi <drakends@yahoo.com>
2018-09-02 01:13:29 +0300

    added huawei models capacities

--- js/sell-stepper.js
+++ js/sell-stepper.js
@@ -16,11 +16,14 @@ function bar_progress(progress_line_object, direction) {
     }
     progress_line_object.attr('style', 'width: ' + new_value + '%;').data('now-value', new_value);
 }
+
 jQuery(document).ready(function() {
 
     $('form fieldset:first').fadeIn('slow');
 
-    $('form .btn-next').on('click', function() {
+    // Step 1 (brand) Next button
+
+    $('form .brand-btn-next').on('click', function() {
         var parent_fieldset = $(this).parents('fieldset');
         var next_step = true;
         var current_active_step = $(this).parents('form').find('.form-wizard.active');
@@ -80,13 +83,12 @@ jQuery(document).ready(function() {
                 });
             }
         }
-
-
-
     });
 
-    // previous step button in step 2
-    $('form .btn-ps2').on('click', function() {
+    // Step 2 (model) buttons
+    // Previous
+
+    $('form .model-btn-previous').on('click', function() {
         var current_active_step = $(this).parents('form').find('.form-wizard.active');
         var progress_line = $(this).parents('form').find('.progress-line');
 
@@ -98,7 +100,153 @@ jQuery(document).ready(function() {
         });
     });
 
-    // previous step button in step 3
+    // Next
+    $('form .model-btn-next').on('click', function() {
+        var parent_fieldset = $(this).parents('fieldset');
+        var next_step = true;
+        var current_active_step = $(this).parents('form').find('.form-wizard.active');
+        var progress_line = $(this).parents('form').find('.progress-line');
+        var model = document.getElementById("sell-wiz").elements["model"];
+        var model_value = model.value;
+
+        if (next_step) {
+            if (model_value == 'Huawei P20 Pro') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp20pro').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P20') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp20').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P20 Lite') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp20l').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P10 Plus') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp10p').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P10') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp10').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P10 Lite') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp10l').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P9 Plus') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp9p').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P9') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp9').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P9 Lite') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp9l').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P8') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp8').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P8 Lite (2017)') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp8l17').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P8 Lite') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hp8l').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei Mate 10 Pro') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hm10p').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei Mate 10') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hm10').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei Mate 9') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hm9').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei Mate 8') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hm8').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            } else if (model_value == 'Huawei P Smart') {
+                parent_fieldset.fadeOut(400, function() {
+                    current_active_step.removeClass('active').addClass('activated').next().addClass('active');
+                    bar_progress(progress_line, 'right');
+                    $(this).nextAll('.capacity-hps').fadeIn();
+                    scroll_to_class($('form'), 20);
+                });
+            }
+        }
+
+    });
+
+    // Step 3 (capacity) buttons
+    // Previous
+    $('form .hc-btn-previous').on('click', function() {
+        var current_active_step = $(this).parents('form').find('.form-wizard.active');
+        var progress_line = $(this).parents('form').find('.progress-line');
+
+        $(this).parents('fieldset').fadeOut(400, function() {
+            current_active_step.removeClass('active').prev().removeClass('activated').addClass('active');
+            bar_progress(progress_line, 'left');
+            $(this).prevAll('.model-huawei').fadeIn();
+            scroll_to_class($('form'), 20);
+        });
+    });
 
     // previous step button in step 4
 
